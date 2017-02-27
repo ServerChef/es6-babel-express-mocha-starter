7494815a81781552ca5b36cced79966d22ad2981
Damodar Dahal <underscoredam@gmail.com>
2017-02-26 22:57:52 -0600

    run tasks successfully

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,17 +1,42 @@
-import express from 'express';
+import express from 'express'
+import * as gulpUtils from '../utils/gulp'
+import GulpRunner from 'gulp-runner'
+
+
+/**
+ * This is the direct reference to the gulp file
+ */
+import gulpFile from '../../gulpfile.js'
+
+
+/**
+ * This will be used to execute gulp tasks
+ * @type {GulpRunner}
+ */
+const gulpRunner = new GulpRunner('gulpfile.js');
+
+
 const router = express.Router();
 
-import getGulpTasks from 'get-gulp-tasks';
-
 router.get('/', (req, res)=>{
-    getGulpTasks('./').then(tasks => {
-        console.log("Tasks: ", tasks);
+    const tasks = gulpUtils.getTasks(gulpFile);
 
-        res.send({'tasks': tasks})
+    res.send({'tasks': tasks})
+});
 
-    }).catch((e)=>{
-        console.log("FAIL", e);
-    })
+
+router.get('/:taskId/execute', (req, res) => {
+    const taskId = req.params.taskId;
+
+    if(!gulpUtils.hasTask(gulpFile, taskId)){
+        throw new Error("Gulp task not found");
+    }
+
+    gulpRunner.run(taskId, (err)=>{
+        res.send(err)
+    });
+
+    res.send({"success": true});
 });
 
 export default router;
